8c23436c44d8d54e49910bb7f9941682f9f477ee
ishvar99 <ishanvarshney99@gmail.com>
2025-11-24 02:59:11 +0530

    finished gemini executor logic

--- components/gemini/executor.ts
+++ components/gemini/executor.ts
@@ -4,6 +4,8 @@ import ky, {Options as KyOptions} from 'ky'
 import Handlebars from 'handlebars'
 import { httpRequestChannel } from "@/inngest/channels/http-request";
 import { geminiChannel } from "@/inngest/channels/gemini";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
 
 Handlebars.registerHelper("json",(context) => new Handlebars.SafeString(JSON.stringify(context,null,2)))
 
@@ -21,5 +23,72 @@ export const GeminiExecutor: NodeExecutor<GeminiData> = async ({data,nodeId, ste
             status: "loading"
         })
     )
-   
+
+    if(!data.variableName){
+        await publish(
+            geminiChannel().status({
+                nodeId: nodeId,
+                status: "error"
+            })
+        )
+        throw new NonRetriableError("Gemini node: Variable name is missing")
+    }
+    if(!data.userPrompt){
+        await publish(
+            geminiChannel().status({
+                nodeId: nodeId,
+                status: "error"
+            })
+        )
+        throw new NonRetriableError("Gemini node: User prompt is missing")
+    }
+   const systemPrompt = data.systemPrompt ? Handlebars.compile(data.systemPrompt)(context) : "You are a helpful assistant."
+   const userPrompt = Handlebars.compile(data.userPrompt)(context);
+
+   const credentialValue = process.env.GOOGLE_GENERATIVE_AI_API_KEY!;
+
+   const google = createGoogleGenerativeAI({
+    apiKey: credentialValue
+   })
+
+   try{
+    const {steps} = await step.ai.wrap(
+        "gemini-generate-text",
+        generateText,
+        {
+            model: google(data.model || "gemini-1.5-flash"),
+            system: systemPrompt,
+            prompt: userPrompt,
+            //for sentry
+            experimental_telemetry: {
+                isEnabled: true,
+                recordInputs: true,
+                recordOutputs: true
+            }
+        }
+    )
+    const text = steps[0]?.content[0]?.type === "text" ? steps[0]?.content[0]?.text : ""
+    await publish(
+        geminiChannel().status({
+            nodeId: nodeId,
+            status: "success"
+        })
+    )
+    return {
+        ...context,
+        [data.variableName!]: {
+            aiResponse: text
+        }
+    }
+   }
+   catch(error){
+    await publish(
+        geminiChannel().status({
+            nodeId: nodeId,
+            status: "error"
+        })
+    )
+    throw error;
+   }
+
 }
